84a647b786a4bd54a31629363691b709c14a7c74
Nick Ezekiel <nickezekias@gmail.com>
2024-11-12 00:07:51 +0100

    fix: added database name in db conf

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'gescamp',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
