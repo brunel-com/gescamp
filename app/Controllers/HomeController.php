d5de5ec6ee85309bea89595c96c50a9830ec6a05
Nick Ezekiel <nickezekias@gmail.com>
2024-11-21 01:29:24 +0100

    refactor home page to use db data

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use App\Models\Chambre;
+use App\Models\Equipement;
+use App\Models\Etudiant;
 use App\Models\Site;
 
 class HomeController extends BaseController
@@ -11,18 +13,23 @@ class HomeController extends BaseController
     {
         $siteModel = model(Site::class);
         $sites = $siteModel->getAll();
+
         $chambreModel = model(Chambre::class);
         $chambres = $chambreModel->getAll();
 
-        $equipements = [];
-        $etudiants = [];
+        $equipementModel = model(Equipement::class);
+        $equipements = $equipementModel->getAll();
+
+        $etudiantModel = model(Etudiant::class);
+        $etudiants = $etudiantModel->getAll();
+        
 
         $data = [
             'chambres' => $chambres,
             'sites' => $sites,
             'equipements' => $equipements,
             'etudiants' => $etudiants,
-            'title' => 'Acceuil'
+            'title' => 'Tableau de bord'
         ];
         return view('home', $data);
     }
--- app/Views/home.php
+++ app/Views/home.php
@@ -7,7 +7,7 @@
     <h4><?= esc($title) ?></h4>
 
     <!-- Section 1: Statistic Cards -->
-    <div class="row g-4 mb-4">
+    <div class="row g-4 mb-4 mt-2">
         <div class="col-md-3">
             <div class="card text-center shadow-sm border-0">
                 <div class="card-body">
@@ -49,21 +49,20 @@
                 <div class="card-body">
                     <h5 class="card-title mb-4">Étudiants et Chambres</h5>
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-6">
                             <h6 class="fw-bold">Étudiants</h6>
                             <ul class="list-group list-group-flush">
-                                <li class="list-group-item">John Doe</li>
-                                <li class="list-group-item">Jane Smith</li>
-                                <li class="list-group-item">Robert Johnson</li>
-                                <li class="list-group-item">Emily Davis</li>
-                                <li class="list-group-item">Michael Brown</li>
+                                <?php foreach ($etudiants as $etudiant): ?>
+                                    <li class="list-group-item"><span><?= esc($etudiant['nom']) ?></span> <span><?= esc($etudiant['prenom']) ?></span></li>
+                                <?php endforeach ?>
                             </ul>
                         </div>
-                        <div class="col-md-6">
+
+                        <div class="col-6">
                             <h6 class="fw-bold">Chambres</h6>
                             <ul class="list-group list-group-flush">
-                                <?php foreach($chambres as $chambre): ?>
-                                    <li class="list-group-item">Chambre <?= esc($chambre['numero']) ?></li>
+                                <?php foreach ($etudiants as $etudiant): ?>
+                                    <li class="list-group-item"><span><?= esc($etudiant['chambre']) ?></span></li>
                                 <?php endforeach ?>
                             </ul>
                         </div>
@@ -80,11 +79,9 @@
                 <div class="card-body">
                     <h5 class="card-title mb-4">Équipements</h5>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Projecteur</li>
-                        <li class="list-group-item">Tableau Blanc</li>
-                        <li class="list-group-item">Chaise</li>
-                        <li class="list-group-item">Table</li>
-                        <li class="list-group-item">Armoir</li>
+                        <?php foreach ($equipements as $equipement): ?>
+                            <li class="list-group-item"><span><?= esc($equipement['type']) ?></span></li>
+                        <?php endforeach ?>
                     </ul>
                 </div>
             </div>
