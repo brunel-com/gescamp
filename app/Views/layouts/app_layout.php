6bbee6879545d69764a194dffc5c16988860ea88
Nick Ezekiel <nickezekias@gmail.com>
2024-11-21 03:57:14 +0100

    refactor

--- app/Views/layouts/app_layout.php
+++ app/Views/layouts/app_layout.php
@@ -5,9 +5,6 @@
     <title>GesCamp</title>
     <link rel="stylesheet" href="/lib/bootstrap-5.3.3-dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="/lib/bootstrap-icons-1.11.3/font/bootstrap-icons.min.css" />
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
-    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"> -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/datatables.net-dt/css/jquery.dataTables.min.css" rel="stylesheet"> -->
     <link href="/css/app.css" rel="stylesheet" />
 </head>
 
@@ -34,14 +31,9 @@
     </main>
 
     <footer class="container mt-5">
-        <em>&copy; Footer Brunel.com 2024</em>
+        <em>&copy; Copyright Brunel.com 2024 - Tout droits reservés</em>
     </footer>
-    <!-- jQuery (required for DataTables) -->
-    <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
-    <!-- DataTables JS -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/datatables.net/js/jquery.dataTables.min.js"></script> -->
     <script src="/lib/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> -->
     <script src="/js/app.js"></script>
 </body>
 
